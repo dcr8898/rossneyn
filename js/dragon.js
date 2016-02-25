d091550b77decbda57ecee856853f71380915a09
Damian C. Rossney <dcrossney@gmail.com>
2016-02-25 09:36:28 -0500

    Update dragon animation

--- js/dragon.js
+++ js/dragon.js
@@ -4,13 +4,17 @@
     var ctx = canvas.getContext('2d');
 
     var controls = document.getElementById('controls')
-    var dragon1Color = 'HotPink';
-    var dragon2Color = 'Purple';
-    var dragon3Color = 'Teal';
-    var dragon4Color = 'Magenta';
-    var timeout = 50;
+    var dragon1Color,
+      dragon1Degree,
+      dragon2Color,
+      dragon2Degree,
+      dragon3Color,
+      dragon3Degree,
+      dragon4Color,
+      dragon4Degree,
+      timeout;
 
-    var maxDegree = 13;
+    var maxDegree = 16;
     var dragonSet = genDragon(maxDegree);
     var startX = 300;
     var startY = 300;
@@ -19,19 +23,19 @@
     var step = 1;
     var lastDraw = null;
 
-    var followPath = function(rotation) {
+    var followPath = function(rotation, degree) {
       var x = startX;
       var y = startY;
       var thRadians = (th * Math.PI) / 180;
       var rotRadians = (rotation * Math.PI) / 180;
       var currentTh = thRadians + rotRadians;
 
-      dragonSet.forEach(function(el) {
-        currentTh += thRadians * el;
+      for (var i = 0, len = Math.pow(2, degree); i < len; i++) {
+        currentTh += thRadians * dragonSet[i];
         x += r * Math.cos(currentTh);
         y += r * Math.sin(currentTh);
         ctx.lineTo(x, y);
-      });
+      }
     };
 
     var draw = function() {
@@ -39,22 +43,22 @@
       ctx.beginPath();
       ctx.strokeStyle = dragon1Color;
       ctx.moveTo(startX, startY);
-      followPath(0);
+      followPath(0, dragon1Degree);
       ctx.stroke();
       ctx.beginPath();
       ctx.strokeStyle = dragon2Color;
       ctx.moveTo(startX, startY);
-      followPath(90);
+      followPath(90, dragon2Degree);
       ctx.stroke();
       ctx.beginPath();
       ctx.strokeStyle = dragon3Color;
       ctx.moveTo(startX, startY);
-      followPath(180);
+      followPath(180, dragon3Degree);
       ctx.stroke();
       ctx.beginPath();
       ctx.strokeStyle = dragon4Color;
       ctx.moveTo(startX, startY);
-      followPath(270);
+      followPath(270, dragon4Degree);
       ctx.stroke();
 
       th += step;
@@ -65,9 +69,13 @@
     var drawWrapper = function(timestamp) {
       timeout = parseInt(controls.elements["timeout"].value);
       dragon1Color = controls.elements["dragon1color"].value;
+      dragon1Degree = controls.elements["dragon1degree"].value;
       dragon2Color = controls.elements["dragon2color"].value;
+      dragon2Degree = controls.elements["dragon2degree"].value;
       dragon3Color = controls.elements["dragon3color"].value;
+      dragon3Degree = controls.elements["dragon3degree"].value;
       dragon4Color = controls.elements["dragon4color"].value;
+      dragon4Degree = controls.elements["dragon4degree"].value;
 
       if (!lastDraw) {
         lastDraw = timestamp;
